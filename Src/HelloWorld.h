595b5b8e98d32db5625a027e682e3ffd0b479f5c
Zeathe <zeathe@qua-axiom.com>
2011-02-04 04:26:13 -0800

    Moving to Projects Sub-Folder

---
+++ Src/HelloWorld.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by zeathe on 9/12/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "HelloWorld.h"
+
+@protocol HelloWorld
+
+@interface HelloWorld : NSView
+{
+}
+@end
+
+@end
---
+++ Src/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by zeathe on 9/6/10.
+//  Copyright Qua Axiom 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	id stringFoo = [[[NSString alloc] init] autorelease];
+	
+
+	
+    return NSApplicationMain(argc,  (const char **) argv);
+}
